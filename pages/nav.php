735042a9c9a851e7ea9eb77dc45d3d910f2cb9f0
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-20 10:25:13 +0900

    navigation bar

--- pages/nav.php
+++ pages/nav.php
@@ -5,7 +5,7 @@
             <li class="nav__links">Website</li>
             <li class="nav__links">Social</li>
             <li class="nav__links">Print</li>
-            <li class="nav__links">Mineal Water</li>
+            <li class="nav__links">Mineral Water</li>
             <li class="nav__links">Welfare</li>
             <li class="nav__links">About</li>
             <li class="nav__contact">Contact Us</li>
